036713c3ba1f571eaa6e40172bf46efcdd432827
mahabbatcomardli <mahabbat.comardli@ibar.az>
2022-12-28 14:54:45 +0400

    modified cell and strings

--- app/src/main/java/com/example/profileapp/MainActivity.kt
+++ app/src/main/java/com/example/profileapp/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,8 +33,12 @@ import androidx.constraintlayout.compose.layoutId
 import com.example.profileapp.extension.currentFraction
 import com.example.profileapp.model.Profile
 import com.example.profileapp.ui.theme.Blue
+import com.example.profileapp.ui.theme.Gray
 import com.example.profileapp.ui.theme.Green
 import com.example.profileapp.ui.theme.ProfileAppTheme
+import com.example.profileapp.utils.Constants.PROFILE_PIC_ID
+import com.example.profileapp.utils.Constants.TYPE
+import com.example.profileapp.utils.Constants.USERNAME
 
 class MainActivity : ComponentActivity() {
 
@@ -139,16 +144,16 @@ fun ProfileHeader(name: String, avatar: Int, type: String, progress: Float) {
             modifier = Modifier
                 .size(200.dp)
                 .clip(CircleShape)
-                .layoutId("profile_pic")
+                .layoutId(PROFILE_PIC_ID)
         )
         Text(
             text = name,
             modifier = Modifier.padding(PaddingValues(top = 20.dp, bottom = 10.dp))
-                .layoutId("username"),
+                .layoutId(USERNAME),
             fontSize = 18.sp,
             fontWeight = FontWeight.Bold
         )
-        Text(text = type, modifier = Modifier.layoutId("type"))
+        Text(text = type, modifier = Modifier.layoutId(TYPE))
     }
 }
 
@@ -174,22 +179,22 @@ fun MoreInfoCard() {
             )
         }
         Text(
-            "More about me",
+            stringResource(R.string.bottomsheet_title),
             fontSize = 18.sp,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(bottom = 4.dp)
         )
         Text(
-            "Swipe up to see more information about me",
+            stringResource(R.string.bottomsheet_subtitle),
             color = Color.Gray,
             modifier = Modifier.padding(bottom = 50.dp)
         )
 
-        BottomSheetCell("Profile type", checkAdmin())
-        BottomSheetCell("My e-mail", profile.email)
-        BottomSheetCell("My phone number", profile.telephone)
-        BottomSheetCell("My gender", profile.gender)
-        BottomSheetCell("My customer number", profile.customerNo)
+        BottomSheetCell(stringResource(R.string.profile_type_title), checkAdmin())
+        BottomSheetCell(stringResource(R.string.email_title), profile.email)
+        BottomSheetCell(stringResource(R.string.phone_title), profile.telephone)
+        BottomSheetCell(stringResource(R.string.gender_title), profile.gender)
+        BottomSheetCell(stringResource(R.string.customer_number_title), profile.customerNo)
     }
 }
 
@@ -199,6 +204,8 @@ private fun BottomSheetCell(title: String, value: String) {
         Text("$title : ", color = Color.Black, fontWeight = FontWeight.Bold)
         Text(value, color = Color.Gray)
     }
+    Divider(thickness = 1.dp, color = Gray)
+    Spacer(modifier = Modifier.size(20.dp))
 }
 
 @ExperimentalMaterialApi
---
+++ app/src/main/java/com/example/profileapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.profileapp.utils
+
+object Constants {
+    const val PROFILE_PIC_ID = "profile_pic"
+    const val USERNAME = "username"
+    const val TYPE = "type"
+}
